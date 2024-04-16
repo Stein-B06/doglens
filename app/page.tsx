5ff3ebbf78dab149744481386554e5a152fdac66
Stein-B06 <Steinb06.kodehode@gmail.com>
2024-04-16 13:15:22 +0200

    updated Tailwind, img

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="">
       <div className={style.wrapper}>
         <div className="text-white mb-10">
-          <h1 className="text-3xl font-bold px-4 py-2 text-center mt-32 ">
+          <h1 className="text-3xl font-bold px-4 py-2 text-center mt-32">
             Bark Zone
           </h1>
 
@@ -16,15 +16,15 @@ export default function Home() {
             Where Furry Faces Steal the Spotlight!
           </p>
         </div>
-        <div className="mt-32 flex flex-col md:flex md:flex-row md:justify-center pl-10  justify-center items-center text-white m-4 ">
+        <div className="mt-24 flex flex-col md:flex md:flex-row md:justify-center pl-10  justify-center items-center text-white m-4 ">
           <Image
             src="/anidogs2.jpeg"
             width={500}
             height={500}
             style={{
-              height: "20vw",
+              height: "25vw",
               objectFit: "cover",
-              width: "20vw",
+              width: "25vw",
               borderRadius: "50%",
               boxShadow:
                 "2px 2px 10px rgba(0, 0, 0, 0.1), 1px 1px 10px rgba(0, 0, 0, 10.08)",
@@ -35,7 +35,7 @@ export default function Home() {
           {/* <div className="text-center">
             <span>Welcome to BarkZone!</span>
           </div> */}
-          <p className="p-12 md:text-xl md:p-16 md:mx-8 text-white">
+          <p className="p-16 md:text-xl md:p-16 md:mx-8 text-white">
             With just a click, you&apos;ll be transported into a world of random
             dog images, each one more adorable than the last. But wait,
             there&apos;s more! With our handy dropdown menu, you can also select
